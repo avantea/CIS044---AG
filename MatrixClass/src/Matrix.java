69a759ce193beb45282a476927a9158722bc85d4
avantea <136939001+avantea@users.noreply.github.com>
2025-09-19 21:34:29 -0700

    Add files via upload

---
+++ MatrixClass/src/Matrix.java
@@ -0,0 +1,74 @@
+import java.util.Random;
+
+public class Matrix {
+    private int[][] data;
+
+    // Constructor with rows and cols
+    public Matrix(int rows, int cols) {
+        data = new int[rows][cols];
+    }
+
+    // Constructor
+    public Matrix(int[][] data) {
+        this.data = new int[data.length][data[0].length];
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[0].length; j++) {
+                this.data[i][j] = data[i][j];
+            }
+        }
+    }
+
+    // Random numbers
+    public void populateRandom() {
+        Random rand = new Random();
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[0].length; j++) {
+                data[i][j] = rand.nextInt(10) + 1;
+            }
+        }
+    }
+
+    // Add
+    public Matrix add(Matrix other) {
+        if (data.length != other.data.length || data[0].length != other.data[0].length) {
+            throw new IllegalArgumentException("Matrices must have the same dimensions to add!");
+        }
+        Matrix result = new Matrix(data.length, data[0].length);
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[0].length; j++) {
+                result.data[i][j] = this.data[i][j] + other.data[i][j];
+            }
+        }
+        return result;
+    }
+
+    // Multiply
+    public Matrix multiply(Matrix other) {
+        if (data[0].length != other.data.length) {
+            throw new IllegalArgumentException("Invalid dimensions");
+        }
+        Matrix result = new Matrix(data.length, other.data[0].length);
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < other.data[0].length; j++) {
+                int sum = 0;
+                for (int k = 0; k < data[0].length; k++) {
+                    sum += this.data[i][k] * other.data[k][j];
+                }
+                result.data[i][j] = sum;
+            }
+        }
+        return result;
+    }
+
+    // Print
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int[] row : data) {
+            for (int val : row) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
